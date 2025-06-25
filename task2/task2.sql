916ffbe80b25cb2dcfb504446b71881a9233f83d
Suraj7987 <rais79411@gmail.com>
2025-06-25 15:26:33 +0530

    Create task2.sql

---
+++ task2/task2.sql
@@ -0,0 +1,4 @@
+-- Task 2: Data Insertion and Null Handling (Library Management System)
+INSERT INTO Student (student_id, name, email) VALUES (1, 'Anjali Mehta', 'anjali@example.com');
+...
+DELETE FROM Book WHERE book_id = 103;
